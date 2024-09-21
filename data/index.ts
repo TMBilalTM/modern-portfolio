1a0d35aa0b7f259dc945b39c9e08aa6d988585cc
TMBilalTM <aksbilalarda@gmail.com>
2024-09-21 16:33:25 +0300

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -180,7 +180,7 @@ export const workExperience = [
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
+    title: "Mobile App Dev",
     desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
     className: "md:col-span-2",
     thumbnail: "/exp2.svg",
